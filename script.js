e647177c6a0a6c67bf24616414b16bc7bbf386a5
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-02-24 08:14:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function replaceAt(str, index, ch) {
   return str.substr(0, index) + ch + str.substr(index + ch.length + 5);
 }
 
-
-
 function typeWriterBeginning(i, txt, id, speed) {
   if (i < txt.length) {
     innerHtml = document.getElementById(id).innerHTML
@@ -23,3 +21,23 @@ function fillIn(txt, id){
   // console.log(spaces);
   document.getElementById(id).innerHTML = spaces
 }
+
+function typeWriter(){
+  let delay = 800;
+  let upperText = "Sometimes The Pros Are Mixed With The Average"; let lowerText = "Gotta Sort And Search!"
+  
+  let id1 = "upperSubtitle"; let id2 = "lowerSubtitle";
+  let speed = 60;
+  console.log(upperText.length);
+  let timeToExectueFirstTypeWriting = upperText.length * speed;
+
+  console.log(document.getElementById("lowerSubtitle"));
+
+  fillIn(upperText, id1);
+  fillIn(lowerText, id2);
+
+  setTimeout(typeWriterBeginning, delay, 0, upperText, id1, speed);
+  setTimeout(typeWriterBeginning, delay*2 + timeToExectueFirstTypeWriting, 0, lowerText, id2, speed);
+}
+
+
