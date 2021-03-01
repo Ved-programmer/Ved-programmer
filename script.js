aaf9f5b7419bb79edba6a5f0cfa4a29215d992a6
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-03-01 12:13:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function fillIn(txt, id){
 }
 
 function typeWriter(){
-  let delay = 800;
+  let delay = 500;
   let upperText = "Sometimes The Pros Are Mixed With The Average"; let lowerText = "Gotta Sort And Search!"
   
   let id1 = "upperSubtitle"; let id2 = "lowerSubtitle";
@@ -39,5 +39,37 @@ function typeWriter(){
   setTimeout(typeWriterBeginning, delay, 0, upperText, id1, speed);
   setTimeout(typeWriterBeginning, delay*2 + timeToExectueFirstTypeWriting, 0, lowerText, id2, speed);
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img.lazy");    
+    var lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop + 150 < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 0);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  });
 
 typeWriter()
